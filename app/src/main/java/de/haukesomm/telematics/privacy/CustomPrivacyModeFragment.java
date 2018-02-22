2e51c9c61dfc00f5eea71e1e21209b5a5e74ffa1
Hauke Sommerfeld <haukesomm@gmail.com>
2018-02-22 13:14:22 +0100

    Add CustomPrivacyModeFragment

---
+++ app/src/main/java/de/haukesomm/telematics/privacy/CustomPrivacyModeFragment.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of the "Telematics App Mockup" project.
+ * Copyright 2017, Hauke Sommerfeld and Sarah Schulz-Mukisa
+ *  
+ * Licensed under the MIT license.
+ * A copy can be obtained under the following link:
+ * https://github.com/haukesomm/Telematics-App-Mockup/blob/master/LICENSE
+ */
+
+package de.haukesomm.telematics.privacy;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import de.haukesomm.telematics.R;
+
+/**
+ * Created on 30.12.17
+ * <p>
+ * This Fragment provides a SettingsFragment used to customize the {@link PrivacyMode#USER_DEFINED}
+ * privacy mode.
+ *
+ * @author Hauke Sommerfeld
+ */
+public class CustomPrivacyModeFragment extends PreferenceFragment {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.fragment_privacymode_custom);
+    }
+
+
+
+    /**
+     * This method enables or disables all Preferences of this PreferenceFragment
+     *
+     * @param enabled   true to enable all Preferences
+     */
+    public void setEnabled(boolean enabled) {
+        for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+            getPreferenceScreen().getPreference(i).setEnabled(enabled);
+        }
+    }
+}
